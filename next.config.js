c2dafbe80a54aedafae98e1d843fa693a6d819de
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-11-28 23:20:57 +0100

    add change on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = {
-  nextConfig,
-  // images: {
-  //   domains: ['asset.brandfetch.io'],
-  // },
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true}
 }
+
+module.exports = nextConfig;
+
