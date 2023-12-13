6a3992e7136855ce02cac27e195c5dc76310efde
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-12-13 13:00:48 +0100

    add svgr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  images: { unoptimized: true}
+  images: { unoptimized: true},
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig;
