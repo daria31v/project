5936d43869d61bc80f4652bdf8438e73e3ac324b
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-12-25 00:29:21 +0100

    add mongoose & mongoDB

---
+++ src/app/auth/signIn/page.tsx
@@ -0,0 +1,39 @@
+import { Container } from '../../components/Container/container';
+import Image from 'next/image';
+import logo from '../../../../public/images/logo.png';
+import Link from 'next/link';
+import { CredentialsForm } from '../../components/CredentialsForm/CredentialsForm';
+import { getServerSession } from 'next-auth';
+import { authConfig } from '../../../lib/authConfig';
+import { redirect } from 'next/navigation';
+
+
+
+export default async function LoginPage() {
+  const session = await getServerSession(authConfig);
+  console.log('session', session);
+  
+
+  if (session) return redirect('/profile');
+
+  return (
+    <Container>
+      <div className="flex justify-center items-center my-10">
+        <div className="bg-gradient-to-r to-[#b0c7f3] via-transparent from-[#E2EAF9] w-[620px] grid justify-center py-6 rounded-md">
+          <div className="text-center">
+            <Image src={logo} alt="logo" width={100} height={40} className="mx-auto" />
+            <h3 className="my-6 text-blue">Log in your account</h3>
+          </div>
+
+          <CredentialsForm />
+          <div className="flex gap-3 justify-center items-center">
+            <p className="text-blue_text text-small font-normal">Don’t have account? </p>
+            <Link href="/registration">
+              <span className="font-semibold text-blue text-small">Register Now</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
--- src/lib/authConfig.ts
+++ src/lib/authConfig.ts
@@ -1,9 +1,11 @@
-import { getServerSession, type NextAuthOptions, type User } from 'next-auth'
+import { getServerSession, type NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 import CredentialProvider from 'next-auth/providers/credentials'
 import { redirect, useRouter } from 'next/navigation'
-import { useSession } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import { connectMongoDB } from './mongodb'
+import User from '@/models/user';
 
 export const authConfig: NextAuthOptions = {
   providers: [
@@ -44,6 +46,36 @@ export const authConfig: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email }) {
+      console.log('Email', email)
+
+      if (account?.provider === 'google') {
+        const { name, email } = user;
+
+        try {
+          await connectMongoDB()
+
+          const userExists = await User.findOne({ email })
+
+          if (!userExists) {
+            const result = await fetch('http://localhost:3000/api/user', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({
+                name,
+                email,
+              }),
+            })
+            if (result.ok) return user
+          }
+        } catch (error) {
+          console.log('error', error)
+        }
+      }
+      return user
+    },
+  },
   // export async function loginIsRequiredServer(){
   //   const session = await getServerSession(authConfig)
   //   if(!session) return redirect ('/')
