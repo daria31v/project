fa8a1a1ae277d4002aab9abd5060edcd88fc2d4f
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2024-01-02 17:12:24 +0100

    fix sentComment

--- src/app/components/Forms/SentCommentForm.tsx
+++ src/app/components/Forms/SentCommentForm.tsx
@@ -9,14 +9,15 @@ export default function SentCommentForm() {
   // const session = getServerSession(authConfig)
   return (
     <div>
-      <form className="md:flex items-center md:gap-6 xl:pl-[55px] 2xl:pl-[100px]">
+      <form className="md:flex items-center md:gap-6 ">
         <div className="">
           <Image src={user} alt="user photo" width={80} height={80} className="mx-auto" />
           <span className="mt-4 inline-block text-base text-primary">Lisa Manobal </span>
         </div>
         <div className="">
-          <textarea placeholder='What are your thoughts?' className="xl:w-[767px] w-44 xl:h-[150px] bg-peach_light rounded-md text-white p-4 placeholder:text-grey_text"></textarea>
-          <div className=" flex gap-3 justify-end items-end w-[767px] mt-2">
+          <textarea placeholder='What are your thoughts?' className="xl:w-[767px] md:w-[600px]  w-44 xl:h-[150px] bg-peach_light rounded-md text-white p-4 placeholder:text-grey_text"></textarea>
+          
+          <div className=" flex gap-3 justify-end items-end xl:w-[767px] md:w-[600px] mt-2">
             <Button className="flex h-[30px] justify-center items-center bg-white border border-orange py-2 px-4 text-orange rounded-3xl hover:text-white hover:bg-gradient-to-l hover:from-[#F38605] hover:via-transparent hover:to-[#F3860542] shadow-md">
               Cancel
             </Button>
