aafdbdaa214e5250548a1f95e470e82fdccb7ddf
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2024-01-04 20:42:58 +0100

    add api on gallarySlider

---
+++ src/utils/new-api-service.js
@@ -0,0 +1,43 @@
+import axios, { AxiosError } from 'axios';
+
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export default class NewsApiService {    
+    constructor() {
+        this.page = 1;
+    }
+    async fetchImage() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const KEY = '31608375-581536e59e6cd039daecb6e21';
+    const value = 'mountain,landscape,sun'
+    const params = 'image_type=photo&per_page=40';
+    const quantityImg = 10;       
+    
+        try {
+            const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${value}&${params}&per_page=${quantityImg}&page=${this.page}`);
+            const data = response.data;
+            // console.log(data);         
+                     
+            if (data.total === 0) {
+                throw new Error('Something went wrong')
+            }
+                                                 
+            this.page += 1; 
+
+            return data;   
+
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return data;
+    }    
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }    
+}
