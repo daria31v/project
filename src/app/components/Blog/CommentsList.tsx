b5cc387af6d324574e54fb51fde243bb0a84ff4f
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2024-01-17 23:59:07 +0100

    change styles texteria and add condition on post page

--- src/app/components/Blog/CommentsList.tsx
+++ src/app/components/Blog/CommentsList.tsx
@@ -1,15 +1,36 @@
-
+'use client'
 import CommentsItem from './CommentsItem'
 import SentCommentForm from '../Forms/SentCommentForm'
+import { getSession, useSession } from 'next-auth/react'
+import { authConfig } from '@/utils/authConfig'
+import Link from 'next/link'
 
 const CommentsList = () => {
+  const { data: session } = useSession()
+
   return (
     <div>
-      <SentCommentForm />
+      {!session ? (
+        <div className="flex gap-2">
+          <p>Please</p>
+          <Link href="/login" className="text-duna">
+            {' '}
+            Login{' '}
+          </Link>
+          <p>or</p>
+          <Link href="/login" className="text-duna">
+            {' '}
+            Join us{' '}
+          </Link>
+          <p>to write a comment </p>
+        </div>
+      ) : (
+        <SentCommentForm />
+      )}
       <ul className="grid gap-5 mt-10">
-      <CommentsItem />
+      {/* {!comments ? (<div><p>Be first who write a comment</p></div>) : (<CommentsItem />)} */}
+        <CommentsItem />
       </ul>
-
     </div>
   )
 }
---
+++ src/app/components/Pagination/Pagination.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pagination = () => {
+  return (
+    <div className="flex">
+        <button>Previos</button>
+        <button>Next</button>
+
+    </div>
+  )
+}
+
+export default Pagination
